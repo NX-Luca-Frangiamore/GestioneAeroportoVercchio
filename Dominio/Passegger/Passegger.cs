be07fda0b087782cdc2591a42966ad44ef9d75c6
Wordluc <40872910+Wordluc@users.noreply.github.com>
2023-10-13 20:59:52 +0200

    add domain project

---
+++ Dominio/Passegger/Passegger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Passegger
+    {
+        public string Name { get; set; }
+        public string Cognome { get; set; }
+        public int Seat { get; set; }
+        public string TypeTicket { get; set; }
+        public List<Luggage> Luggages { get; set; }
+    }
+}
--- GestioneAeroporto/Commands/CreatePassegger/CreatePasseggerCommand.cs
+++ GestioneAeroporto/Commands/CreatePassegger/CreatePasseggerCommand.cs
@@ -5,11 +5,17 @@
         public required string Nome { get; init; }
         public required string Cognome { get; init; }
         public List<Luggage>? Luggages { get; init; }
+        public TypeClass TypeTicket { get; init; }
+        public int NSeat { get; set; }
     }
 
     class Luggage
     {
         public float Peso {  get; init; }
         public float Dimensione {  get; init; }
+    }
+    enum TypeClass
+    {
+        Second,Primary
     }
 }
