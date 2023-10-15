d285eaec9164fc7e9ec0608275cd8286278c98a6
Wordluc <40872910+Wordluc@users.noreply.github.com>
2023-10-15 17:14:58 +0200

    Refactoring repository

---
+++ GestioneAeroporto/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dominio;
+
+namespace Core
+{
+    public interface IRepository
+    {
+         Task<Route> NewAccoutPassegger(Passegger person);
+         Task<bool> DeleteAccoutPassegger(string id);
+         Task<bool> ChangeAccoutPassegger(string id, Passegger person);
+         Task<Route> NewRoute(Aereo aereo, City from, City to);
+         Task<bool> DeleteRoute(Route route);
+         Task<bool> ChangeRoute(string id, Route newRoute);
+    }
+}
